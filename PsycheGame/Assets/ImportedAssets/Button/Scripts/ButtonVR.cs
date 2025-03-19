2da40466073ac3be4a98cfac572f1a19cf7dcd7e
avashilling <165050625+avashilling@users.noreply.github.com>
2025-03-19 19:11:29 -0400

    Working keypad
    Keypad works with ray interactors, doesn't have correct code logic yet

--- PsycheGame/Assets/ImportedAssets/Button/Scripts/ButtonVR.cs
+++ PsycheGame/Assets/ImportedAssets/Button/Scripts/ButtonVR.cs
@@ -1,99 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-[RequireComponent(typeof(XRSimpleInteractable))]
+[RequireComponent(typeof(Collider))]
 public class ButtonVR : MonoBehaviour
 {
     [Header("Button Settings")]
-    // The visual part of the button that moves when pressed
-    public GameObject button;
-
-    // Optional events to hook up extra behavior from the Inspector
-    public UnityEvent onPress;
-    public UnityEvent onRelease;
+    public GameObject buttonVisual; // The visual button that moves when pressed
+    public UnityEvent onPress; // Optional events for additional behavior
+    public UnityEvent onRelease; // Optional events for additional behavior
 
     private AudioSource sound;
-    private bool isPressed;
+    private bool isPressed = false;
 
-    // Reference to the interactable component (XRSimpleInteractable)
-    private XRSimpleInteractable simpleInteractable;
-
-    void Awake()
+    private void Awake()
     {
         sound = GetComponent<AudioSource>();
-        // Get the interactable component (it must be on the same GameObject)
-        simpleInteractable = GetComponent<XRSimpleInteractable>();
-        if (simpleInteractable == null)
-        {
-            Debug.LogWarning("XRSimpleInteractable component not found! Please add it to the button GameObject.");
-        }
     }
 
-    void OnEnable()
-    {
-        // Subscribe to the built-in events
-        if (simpleInteractable != null)
-        {
-            simpleInteractable.selectEntered.AddListener(OnSelectEntered);
-            simpleInteractable.selectExited.AddListener(OnSelectExited);
-        }
-    }
-
-    void OnDisable()
-    {
-        // Unsubscribe from the events
-        if (simpleInteractable != null)
-        {
-            simpleInteractable.selectEntered.RemoveListener(OnSelectEntered);
-            simpleInteractable.selectExited.RemoveListener(OnSelectExited);
-        }
-    }
-
-    private void OnSelectEntered(SelectEnterEventArgs args)
+    // This function is called when the button is pressed
+    public void PressButton()
     {
         if (!isPressed)
         {
-            PressButton();
+            buttonVisual.transform.localPosition = new Vector3(0, 0.003f, 0); // Simulate button press
+            onPress.Invoke(); // Invoke events
+            if (sound != null)
+            {
+                sound.Play();
+            }
+            isPressed = true;
         }
     }
 
-    private void OnSelectExited(SelectExitEventArgs args)
+    // This function is called when the button is released
+    public void ReleaseButton()
     {
         if (isPressed)
         {
-            ReleaseButton();
+            buttonVisual.transform.localPosition = new Vector3(0, 0.015f, 0); // Simulate button release
+            onRelease.Invoke(); // Invoke events
+            isPressed = false;
         }
-    }
-
-    private void PressButton()
-    {
-        // Move the button to simulate a press
-        button.transform.localPosition = new Vector3(0, 0.003f, 0);
-        onPress.Invoke();
-        if (sound != null)
-        {
-            sound.Play();
-        }
-        isPressed = true;
-    }
-
-    private void ReleaseButton()
-    {
-        // Reset the button position to simulate a release
-        button.transform.localPosition = new Vector3(0, 0.015f, 0);
-        onRelease.Invoke();
-        isPressed = false;
-    }
-
-    public void SpawnSphere()
-    {
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-        sphere.transform.localPosition = new Vector3(0, 1, 2);
-        sphere.AddComponent<Rigidbody>();
     }
 }
